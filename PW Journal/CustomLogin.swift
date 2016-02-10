1f730b89194bb5103a27ac2ecc64cec295184a56
Nishant Punia <nishantpunia3171@gmail.com>
2016-02-10 20:41:31 +0530

    Initializing the project

---
+++ PW Journal/CustomLogin.swift
@@ -0,0 +1,19 @@
+//
+//  CustomLogin.swift
+//  PW Journal
+//
+//  Created by nishant punia on 08/02/16.
+//  Copyright © 2016 MLBNP. All rights reserved.
+//
+
+import UIKit
+
+class CustomLogin: UIView {
+
+    
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+
+}
---
+++ PW Journal/EmailLoginController.swift
@@ -0,0 +1,18 @@
+//
+//  EmailLoginController.swift
+//  PW Journal
+//
+//  Created by nishant punia on 10/02/16.
+//  Copyright © 2016 MLBNP. All rights reserved.
+//
+
+import UIKit
+
+class EmailLoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
